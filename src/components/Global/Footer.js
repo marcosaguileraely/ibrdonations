67bc487def57084b61e616d102a3c116930e0418
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-04-08 19:16:14 -0500

    Footer changes

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -18,30 +18,25 @@ class Footer extends Component {
     //const { copyright } = this.props;
 
     return (
-      <div className="Footer">
-        {/* <h4>{ copyright }</h4> */}
-        
+      <div className="Footer">        
         <div class="pt-5">
           <div class="container">
             <div class="row"> </div>
           </div>
         </div>
 
-        <nav class="navbar navbar-expand-md my-3 navbar-light bg-light">
+        <nav class="navbar navbar-expand-md my-3 navbar-light bg-light" id="footer_bar">
           <div class="container">
-            <a class="navbar-brand" href="#"><b>Navbar Light</b></a>
+            <a class="navbar-brand" href="http://renacerib.org" target="_blank" rel="noopener noreferrer">
+                <i class="fa d-inline fa-home"></i><b>&nbsp;renaceribr.org </b>
+            </a>
+                <i class="fa d-inline fa-phone-square"></i><b>&nbsp;Tel:  (+571) 5268021 </b>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarLightSupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span> </button>
             <div class="collapse navbar-collapse text-center justify-content-end" id="navbarLightSupportedContent">
               <ul class="navbar-nav">
                 <li class="nav-item mx-1">
-                  <a class="nav-link" href="#"><i class="fa d-inline fa-facebook-square"></i>&nbsp; share</a>
-                </li>
-                <li class="nav-item mx-1">
-                  <a class="nav-link" href="#"><i class="fa d-inline fa-twitter"></i>&nbsp; tweet</a>
-                </li>
-                <li class="nav-item mx-1">
-                  <a class="nav-link" href="#"><i class="fa d-inline fa-pinterest"></i>&nbsp; pin</a>
+                  <a class="nav-link" href="#"><i class="fa d-inline fa-facebook-square"></i>&nbsp; Facebook</a>
                 </li>
               </ul>
             </div>
