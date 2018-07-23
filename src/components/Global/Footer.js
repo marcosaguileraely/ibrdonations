0114cbcfd639f49b1a603278e79b728f76e91ab8
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-07-22 23:03:19 -0500

    Footer changed

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -19,24 +19,24 @@ class Footer extends Component {
 
     return (
       <div className="Footer">        
-        <div class="pt-5">
-          <div class="container">
-            <div class="row"> </div>
+        <div className="pt-5">
+          <div className="container">
+            <div className="row"> </div>
           </div>
         </div>
 
-        <nav class="navbar navbar-expand-md my-3 navbar-light bg-light" id="footer_bar">
-          <div class="container">
-            <a class="navbar-brand" href="http://renacerib.org" target="_blank" rel="noopener noreferrer">
-                <i class="fa d-inline fa-home"></i><b>&nbsp;renaceribr.org </b>
+        <nav className="navbar navbar-expand-md my-3 navbar-light bg-light" id="footer_bar">
+          <div className="container">
+            <a className="navbar-brand" href="http://renacerib.org" target="_blank" rel="noopener noreferrer">
+                <i className="fa d-inline fa-home"></i><b>&nbsp;renaceribr.org </b>
             </a>
-                <i class="fa d-inline fa-phone-square"></i><b>&nbsp;Tel:  (+571) 5268021 </b>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarLightSupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span> </button>
-            <div class="collapse navbar-collapse text-center justify-content-end" id="navbarLightSupportedContent">
-              <ul class="navbar-nav">
-                <li class="nav-item mx-1">
-                  <a class="nav-link" href="https://www.facebook.com/renacerib/" target="_blank" rel="noopener noreferrer"><i class="fa d-inline fa-facebook-square"></i>&nbsp; Facebook</a>
+                <i className="fa d-inline fa-phone-square"></i><b>&nbsp;Tel:  (+571) 5268021 </b>
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarLightSupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span> </button>
+            <div className="collapse navbar-collapse text-center justify-content-end" id="navbarLightSupportedContent">
+              <ul className="navbar-nav">
+                <li className="nav-item mx-1">
+                  <a className="nav-link" href="https://www.facebook.com/renacerib/" target="_blank" rel="noopener noreferrer"><i className="fa d-inline fa-facebook-square"></i>&nbsp; Facebook</a>
                 </li>
               </ul>
             </div>
